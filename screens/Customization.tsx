1d9eee4fcc4c5d16e298a281226a876af83fc8c7
daolove0323 <daolove0323@gmail.com>
2024-11-27 16:48:33 +0900

    feat: 사용자화 뷰 수정

--- screens/Customization.tsx
+++ screens/Customization.tsx
@@ -12,36 +12,33 @@ import {
 
 function Customization({}): React.JSX.Element {
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState<Chat[]>([]);
+  const [data, setData] = useState<Customization[]>([]);
   const [newMessage, setNewMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const chatData = await getChatData();
-        setData(chatData);
+        const CustomizationData = await getCustomization();
+        setData(CustomizationData);
       } catch (error) {
-        console.error('메시지 데이터를 가져오는 중 오류 발생:', error);
+        console.error('사용자화 데이터를 가져오는 중 오류 발생:', error);
       } finally {
         setLoading(false);
       }
     };
-
     fetchData();
   }, []);
 
-  const renderItem = ({item}: {item: Chat}) => renderChat(item);
+  const renderItem = ({item}: {item: Customization}) => renderCustomization(item);
 
   const handleSendMessage = async () => {
     if (newMessage.trim()) {
-      const response = await sendMessage(newMessage);
-
+      const response = await addCustomizaiton(newMessage);
       console.log(response);
-
       setNewMessage('');
       const updatedData = [
         ...data,
-        {id: String(data.length + 1), sender: '인형', message: newMessage},
+        {id: String(data.length + 1), requirement: newMessage},
       ];
       setData(updatedData);
     }
@@ -61,7 +58,7 @@ function Customization({}): React.JSX.Element {
       <View style={styles.messageSendContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder="메시지를 입력하세요"
+          placeholder="요구사항을 입력하세요."
           value={newMessage}
           onChangeText={setNewMessage}
         />
@@ -73,24 +70,25 @@ function Customization({}): React.JSX.Element {
   );
 }
 
-interface Requirement {
+interface Customization {
   id: string;
   requirement: string;
 }
 
-const getChatData = async (): Promise<Requirement[]> => {
+const getCustomization = async (): Promise<Customization[]> => {
   // const response = await fetch('https://reactnative.dev/movies.json');
   const json = {
     data: [
-      {id: '1', requirement: '인형사용자화'},
-      {id: '2', requirement: '인형사용자화'},
-      {id: '3', requirement: '인형사용자화'} 
-    ]   
+      {id: '1', requirement: '인형 이름은 말랑핑이야.'},
+      {id: '2', requirement: '아이 이름은 예솔이야.'},
+      {id: '3', requirement: '예솔이 또래 친구처럼 이야기해줘.'},
+      {id: '3', requirement: '유치원에서 어땠는지 매일 물어봐.'}
+    ],
   };
   return json.data;
 };
 
-const sendMessage = async (message: string) => {
+const addCustomizaiton = async (requirement: string) => {
   try {
     const response = await fetch(
       'https://your-server-endpoint.com/sendMessage',
@@ -100,36 +98,53 @@ const sendMessage = async (message: string) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          message: message,
+          requirement: requirement,
         }),
       },
     );
 
     if (!response.ok) {
-      throw new Error('서버에 메시지 전송 실패');
+      throw new Error('요구사항 추가 실패');
     }
-
     return await response.json();
   } catch (error) {
-    console.error('메시지 전송 중 오류 발생:', error);
+    console.error('요구사항 추가 중 오류 발생:', error);
   }
 };
 
-const renderChat = (chatProp: Chat) => {
+const renderCustomization = (customizationProp: Customization) => {
   return (
-    <View
-      style={[
-        styles.messageContainer,
-        chatProp.sender === '인형' ? styles.dollMessage : styles.myMessage,
-      ]}>
-      <Text style={styles.senderText}>{chatProp.sender}</Text>
-      <Text style={styles.messageText}>{chatProp.message}</Text>
-      <Text style={styles.dateText}>2024.10.11 18:00</Text>
+    <View style={styles.messageContainer}>
+      <View style={styles.textContainer}>
+        <Text style={styles.requirementText}>{`• ${customizationProp.requirement}`}</Text>
+      </View>
+      <TouchableOpacity style={styles.iconButton} onPress={() => console.log('Button Pressed')}>
+        <Text>삭제버튼</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  messageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  requirementText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  iconButton: {
+    padding: 5
+  },
   messageSendContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -161,34 +176,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-  messageContainer: {
-    padding: 10,
-    marginVertical: 8,
-    maxWidth: '70%',
-    borderRadius: 10,
-  },
-  myMessage: {
-    alignSelf: 'flex-end',
-    backgroundColor: '#dcf8c6',
-  },
-  dollMessage: {
-    alignSelf: 'flex-start',
-    backgroundColor: '#f0f0f0',
-  },
-  senderText: {
-    fontSize: 12,
-    color: '#888',
-    marginBottom: 4,
-  },
-  messageText: {
-    fontSize: 16,
-    color: '#333',
-  },
   dateText: {
     alignSelf: 'flex-end',
     fontSize: 10,
     color: '#888',
-  }
+  },
 });
 
 export default Customization;
